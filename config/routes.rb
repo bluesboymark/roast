d58b4560b642cfed5cd4e2f115a299fa6f142543
nicholas anggiansah <Mark@Hadys-MacBook-Pro.local>
2017-06-27 09:25:24 -0700

    app working with list of posts

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,36 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  root 'posts#index'
+  get 'posts/' => 'posts#index', as: :posts
+
+  get 'posts/:id' => 'posts#show', as: :post
+
+  get 'posts/new'
+
+  get 'posts/create'
+
+  get 'posts/edit'
+
+  get 'posts/update'
+
+  get 'posts/destroy'
+
+
+  resources :user
+  # get 'users/index' => 'users#index', as: :products
+  #
+  # get 'users/show' => 'users#show', as :show
+  #
+  # get 'users/new' => ''
+  #
+  # get 'users/create'
+  #
+  # get 'users/edit'
+  #
+  # get 'users/update'
+  #
+  # get 'users/destroy'
+
+  get '/login' => 'sessions#new', as: :login
+  post '/login' => 'sessions#create'
+  delete '/logout' => 'sessions#destroy', as: :logout
 end
